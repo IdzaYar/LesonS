086c095214623ddaea17b2dc29d17be088570a10
IdzaYar <160059368+IdzaYar@users.noreply.github.com>
2024-03-13 19:03:07 +0200

    Add files via upload

---
+++ HomeWork16.py
@@ -0,0 +1,6 @@
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
+name = input("Введіть ім'я: ")
+age = int(input("Введіть вік: "))
+result = say_hi(name, age)
+print(result)
